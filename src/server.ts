122e3d246397dc7bc14a5177eca3fdf69f3aa385
Marcos Corsi <marcosvcorsi@gmail.com>
2021-06-28 21:26:26 -0300

    feat: add server and scripts

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => res.send('OK'));
+
+const port = process.env.PORT || 3000;
+
+app.listen(port,  () => console.log(`Server is running on port ${port}`))
