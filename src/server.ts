22104e41ccc73fe5d2e3edf18679091d9f593d26
Marcos Corsi <marcosvcorsi@gmail.com>
2021-06-29 21:33:20 -0300

    feat: add socket io and public

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import { join } from 'path';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 
 const app = express();
 
-app.get('/', (req, res) => res.send('OK'));
+app.use(express.static(join(__dirname, '..', 'public')));
+
+const server = createServer(app);
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+  console.log(socket.id);
+})
 
 const port = process.env.PORT || 3000;
 
-app.listen(port,  () => console.log(`Server is running on port ${port}`))
+server.listen(port,  () => console.log(`Server is running on port ${port}`))
